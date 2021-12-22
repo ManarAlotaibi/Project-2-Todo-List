5315d756e1c2c0d1903526c79bf5c82c57e8769a
ManarAlotaibi <48811099+ManarAlotaibi@users.noreply.github.com>
2021-12-22 08:57:34 +0300

    finish get and post

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,35 @@ const app = express()
 
 const db = require('./db')
 const Todo = require('./todo')
+// console.log(Todo);
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json('GET / IS WORKING')
 
 })
 
-
 app.get('/tasks', (req, res) => {
-    res.json('GET / IS WORKING')
+    Todo.find({},(err,data)=>{
+        if(err) {
+            console.log('ERROR: ',err);
+        }else{
+            res.json(data)
+        }
+    })   
+})
 
+app.post('/tasks', (req, res) => {
+    console.log('26: ',req.body)
+
+    Todo.create(req.body,(err,newTask)=>{
+        if(err) {
+            console.log('ERROR: ',err);
+        }else{
+            res.status(201).json(newTask)
+        }
+    })  
 })
 
 
